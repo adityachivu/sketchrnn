e318b6accfe9320407ba9ca71b143d067e48588a
Aditya Chivukula <aditya.chivukula@gmail.com>
2018-08-27 11:57:47 +0000

    skeleton

---
+++ model.py
@@ -0,0 +1,63 @@
+import torch
+from torch import nn
+
+
+class EncoderRNN(nn.module):
+    """
+
+    """
+    def __init__(self):
+        """
+
+        """
+        self.bi_gru = torch.nn.GRU(input_size)
+
+    def forward(self, x):
+        """
+
+        :param x:
+        :return:
+        """
+
+
+class DecoderRNN(nn.module):
+    """
+
+    """
+    def __init__(self):
+        """
+
+        """
+
+    def forward(self, x):
+        """
+
+        :param x:
+        :return:
+        """
+
+
+class SeqVAE(nn.module):
+    """
+
+    """
+    def __init__(self):
+        """
+
+        """
+        super(SeqVAE).__init__()
+
+
+    def model(self, x):
+        """
+
+        :param x:
+        :return:
+        """
+
+    def guide(self, x):
+        """
+
+        :param x:
+        :return:
+        """
